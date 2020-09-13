dfdcd9636b9ddf8b4e63cb7b35e4286493665a25
Lisandro Scarrone <lisandro.sebsca@gmail.com>
2020-09-13 18:26:39 -0300

    Add Manage Cards section
    When clicking on a card the .target varies between the three inner components of the card and the main card div itself depending on where it is clicked.
    Add a function to manage all cases.

--- src/index.js
+++ src/index.js
@@ -35,6 +35,8 @@ function setImageClass() {
     $flipCardBack.forEach((card) => {
         const image = getRandomImage();
         card.classList.add(image);
+        const $parent = card.parentNode.parentNode;
+        $parent.classList.add(`_${image}`);
     });
 }
 
@@ -48,10 +50,42 @@ function resetCard() {
     $flipCardBack.forEach((card) => {
         images.forEach((element) => {
             card.classList.remove(element);
+            const $parent = card.parentNode.parentNode;
+            $parent.classList.remove(`_${element}`);
         });
     });
 }
 
 function resetImagesDouble() {
     imagesDouble = images.concat(images);
+}
+
+////////// Manage Cards //////////
+
+let $firstCard;
+
+function manageBoard() {
+    const $board = document.querySelector('.board');
+    $board.onclick = function (event) {
+        const $element = event.target;
+        if ($element.classList.contains('flip-card-back')) {
+            manageCards($element.parentNode.parentNode);
+        }
+        if ($element.classList.contains('flip-card-front')) {
+            manageCards($element.parentNode.parentNode);
+        }
+        if ($element.classList.contains('flip-card-inner')) {
+            manageCards($element.parentNode);
+        }
+        if ($element.classList.contains('flip-card')) {
+            manageCards($element);
+        } else {
+            return;
+        }
+    };
+}
+
+function manageCards() {
+    if (!$firstCard) {
+    }
 }
