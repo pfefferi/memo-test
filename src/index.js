fa6db4b4348bec2ff64cfd1ac61f72006c939060
Lisandro Scarrone <lisandro.sebsca@gmail.com>
2020-09-18 12:55:02 -0300

    Add Score and Timer sections

--- src/index.js
+++ src/index.js
@@ -23,7 +23,9 @@ function blockUserInput() {
 }
 
 function startGame() {
+    stopTimer();
     resetBoard();
+    resetScore();
     resetFirstCard();
     blockUserInput();
     setTimeout(() => {
@@ -32,6 +34,7 @@ function startGame() {
         setImageClass();
         userInput();
         manageBoard();
+        startTimer();
     }, 500);
 }
 
@@ -73,6 +76,8 @@ function resetBoard() {
     document.querySelector('.board-parent').classList.remove('hide');
     document.querySelector('.success').classList.add('hide');
     document.querySelector('.success-img').classList.add('hide');
+    document.querySelector('.score-div').classList.add('hide');
+
     success = 0;
 }
 
@@ -142,12 +147,16 @@ function compareCards(first, second) {
         animationMatch(first, second);
         firstCard = null;
         secondCard = null;
+        attempts++;
+        updateAttempts();
         success++;
         checkSuccess();
     } else {
-        animationNotMatch(first, second);
+        animationNoMatch(first, second);
         firstCard = null;
         secondCard = null;
+        attempts++;
+        updateAttempts();
     }
 }
 
@@ -169,7 +178,7 @@ function animationMatch(first, second) {
     });
 }
 
-function animationNotMatch(first, second) {
+function animationNoMatch(first, second) {
     const $flipCard = document.querySelectorAll('#flipCard');
     $flipCard.forEach((element) => {
         if (element.parentNode === first) {
@@ -195,6 +204,93 @@ function checkSuccess() {
             document.querySelector('.board-parent').classList.add('hide');
             document.querySelector('.success').classList.remove('hide');
             document.querySelector('.success-img').classList.remove('hide');
+            document.querySelector('.score-div').classList.remove('hide');
+            updateScore();
+            stopTimer();
+            game = false;
         }, 2500);
     }
+}
+
+////////// Score //////////
+
+let attempts = 0;
+let bestScore = 9999;
+
+let time = '';
+let bestTime = '59:59';
+
+function updateAttempts() {
+    document.querySelector('#attempts').textContent = attempts;
+}
+
+function resetScore() {
+    attempts = 0;
+    time = 0;
+    updateAttempts();
+}
+
+function updateScore() {
+    document.querySelector('.attempts').textContent = `Intentos: ${attempts}`;
+    /*if (attempts < bestScore) {
+        bestScore = attempts;
+        document.querySelector(
+            '.best-attempts'
+        ).textContent = `${bestScore} intentos`;
+    }*/
+
+    document.querySelector('.time').textContent = `Tiempo: ${time}`;
+    /*if (time <= bestTime) {
+        // es string, no numero
+        bestTime = time;
+        document.querySelector('.best-time').textContent = `Tiempo: ${time}`;
+    }
+    if (bestTime === '59:59') {
+        document.querySelector('.best-time').textContent = `Tiempo: ${time}`;
+    }*/
+}
+
+////////// Timer //////////
+
+let timerFunction;
+let timeContainer = document.querySelector('#time');
+let seconds = 0;
+let minutes = 0;
+
+function timer() {
+    if (seconds === 59) {
+        seconds = 0;
+        minutes++;
+        if (seconds > 9) {
+            time = `${minutes}:${seconds}`;
+            timeContainer.innerHTML = time;
+        } else {
+            time = `${minutes}:0${seconds}`;
+            timeContainer.innerHTML = time;
+        }
+    } else {
+        seconds++;
+        if (minutes > 0) {
+            if (seconds > 9) {
+                time = `${minutes}:${seconds}`;
+                timeContainer.innerHTML = time;
+            } else {
+                time = `${minutes}:0${seconds}`;
+                timeContainer.innerHTML = time;
+            }
+        } else {
+            time = `${seconds}`;
+            timeContainer.innerHTML = time;
+        }
+    }
+}
+
+function startTimer() {
+    timerFunction = setInterval(timer, 300);
+}
+
+function stopTimer() {
+    seconds = -1;
+    minutes = 0;
+    clearInterval(timerFunction);
 }
