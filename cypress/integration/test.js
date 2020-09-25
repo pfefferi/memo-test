3198809bef00b834437531c9d5c088f2c6a0b338
Lisandro Scarrone <lisandro.sebsca@gmail.com>
2020-09-25 12:03:06 -0300

    Finish cypress test

--- cypress/integration/test.js
+++ cypress/integration/test.js
@@ -15,29 +15,67 @@ context('Memotest', () => {
         });
 
         it('Checks if the cards are randomly generated', () => {
-            cy.get('.cardContainer').then((card) => {
-                let originalClasses = [];
-                card.each(function (i, card) {
-                    originalClasses.push(card.className);
+            cy.get('.button').click();
+            let originalClasses = [];
+            cy.get('.cardContainer').then((cards) => {
+                cards.each((i, card) => {
+                    originalClasses.push(card.classList);
+                    //console.log(card.classList);
                 });
+            });
 
-                cy.visit(URL);
+            cy.visit(URL);
+            cy.get('.button').click();
+            let newClasses = [];
+            cy.get('.cardContainer').then((newCards) => {
+                newCards.each((i, card) => {
+                    newClasses.push(card.classList);
+                });
+                //console.log(originalClasses, newClasses);
+                cy.wrap(originalClasses).should('not.deep.equal', newClasses);
+            });
+        });
+        it('chooses a wrong combination of cards', () => {
+            cy.visit(URL);
+            cy.get('.button').click();
+            cy.wait(1000);
 
-                cy.get('.button').click();
-
-                let newClasses = [];
-                cy.get('.cardContainer').then((newCards) => {
-                    newCards.each(function (i, card) {
-                        newClasses.push(card.className);
+            cy.get('.cardContainer').then((cards) => {
+                console.log(cards[0].classList[1]);
+                let pairs = getPairs(cards);
+                let cardPairs = Object.values(pairs);
+                cy.get(cardPairs[0][0]).click();
+                cy.get(cardPairs[1][0]).click();
+                cy.wait(1000);
+            });
+        });
+        it('finishes the game', () => {
+            cy.get('.cardContainer').then((cards) => {
+                let cardPairs = getPairs(cards);
+                const newLocal = Object.values(cardPairs);
+                cy.get(newLocal).then((pairs) => {
+                    pairs.each((i, pair) => {
+                        cy.get(pair[0]).click();
+                        cy.get(pair[1]).click();
                     });
-
-                    cy.wrap(originalClasses).should(
-                        'not.deep.equal',
-                        newClasses
-                    );
+                    cy.wait(1000);
                 });
             });
         });
-        asd;
     });
 });
+
+function getPairs(cards) {
+    let cardPairs = {};
+
+    cards.each((i, card) => {
+        const cardClass = card.classList[1];
+        const exists = cardPairs[cardClass];
+        if (exists) {
+            cardPairs[cardClass].push(card);
+        } else {
+            cardPairs[cardClass] = [card];
+        }
+    });
+    return cardPairs;
+}
